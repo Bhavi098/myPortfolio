a090fd9e010444db5a80118079b8509f7836df71
Bhavi098 <132342097+Bhavi098@users.noreply.github.com>
2025-07-25 11:31:12 +0530

    edited contents

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+import console from "../../Assets/Projects/console.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import event from "../../Assets/Projects/events.jpg";
+import todo from "../../Assets/Projects/todo.jpg";
+import blog from "../../Assets/Projects/blog.jpg";
+import gym from "../../Assets/Projects/gym.jpg";
 
 function Projects() {
   return (
@@ -23,60 +23,63 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={todo}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Todo-application"
+              description=" ToDo App is a full-stack task management application built with MongoDB, Express, React, and Node.js. It allows users to add, update, delete, and mark tasks as complete with a responsive UI and persistent storage."
+              ghLink="https://github.com/Bhavi098/Todo-application"
+              demoLink="#"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={blog}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Blog-Application"
+              description="MERN Blog is a full-stack open-source blogging platform built using MongoDB, Express, React, and Node.js. It features authentication, CRUD operations, image uploads, comments, and a responsive UI."
+              ghLink="https://github.com/Bhavi098/Blog-application"
+              demoLink="#"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={console}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Hotel-System"
+              description=" Developed a console based java application. The main
+ feauture of the app is in it's simplicity and use of core OOP
+ feautures like Abstraction, Polymorphism, Encapsulation
+ and Inheritance."
+              ghLink="https://github.com/Bhavi098/Hotel_room_management_java"
+              demoLink="#"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={event}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Eventoems"
+              description=" EventoEMS is a full-featured MERN stack event management system designed for campus events — enabling scheduling, approval, ticket booking, and QR-based entry all in one platform."
+              ghLink="https://github.com/Bhavi098/Event-management-system"
+              demoLink="#"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={gym}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Workout Assistant"
+              description="Built with TensorFlow.js and MoveNet, it ensures privacy, supports custom workouts, and enables users to train and test models right from the browser."
+              ghLink="https://github.com/Bhavi098/workout-assistant"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
@@ -86,7 +89,7 @@ function Projects() {
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
-          </Col>
+          </Col> */}
         </Row>
       </Container>
     </Container>
